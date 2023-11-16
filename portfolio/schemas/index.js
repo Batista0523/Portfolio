14bad5907c0dba42d1173a0086326782ad3f2780
elisaul batista <elisaulbatista@pursuit.org>
2023-11-16 11:29:37 -0500

    added schema with sanity

---
+++ portfolio/schemas/index.js
@@ -0,0 +1,10 @@
+import abouts from './abouts'
+import testimonials from './testimonials'
+import brands from './brands'
+import contact from './contact'
+import skills from './skills'
+import works from './works'
+import workExperience from './workExperience'
+import experiences from './experiences'
+
+export const schemaTypes = [testimonials,abouts,brands,contact,skills,workExperience,works,experiences]
